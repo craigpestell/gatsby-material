8e2811ff3c27e48c1f6f45272d269cb9663ad97b
Craig Pestell <craigpestell@gmail.com>
2018-10-21 11:17:15 -0700

    adding pm2 deploy script

---
+++ ecosystem.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'npm',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'run',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    development : {
+      user : 'repn',
+      
+      arguments: ['run', 'dev-server'],
+      host : 'kali-dev',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/home/repn/workspace/kali-posts-admin',
+      'post-deploy' : 'npm install && pm2 startOrReload ecosystem.config.js --env development'
+    },
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
