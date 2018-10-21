bf37bbeb8c7ef67d90184c849d2f3607b48c1137
Craig Pestell <craigpestell@gmail.com>
2018-10-21 12:43:33 -0700

    fixing pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'Posts Admin (CMS)',
       script: 'npm',
-      args: 'dev-server',
+      args: ['run dev-server'],
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
