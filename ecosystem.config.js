6a1efd63efbc2e61ae3c36ec95eee5a7d19b2f80
Craig Pestell <craigpestell@gmail.com>
2018-10-21 12:04:24 -0700

    fixing pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
     development: {
       user: 'repn',
 
-      arguments: ['run', 'dev-server'],
+      arguments: ['dev-server'],
       host: 'kali-dev',
       ref: 'origin/master',
       repo: 'git@github.com:craigpestell/kali-posts-admin.git',
