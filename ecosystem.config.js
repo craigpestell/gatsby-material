4729452f77e063ce4c4f05d7427687e037b27bd7
Craig Pestell <craigpestell@gmail.com>
2018-10-21 11:27:47 -0700

    adding pm2 deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'API',
+      name: 'Posts Admin (CMS)',
       script: 'npm',
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
